caddd2793897620e9bfad193f9be3d1b1b6951d4
Raj <40709274+Raj00377@users.noreply.github.com>
2019-06-11 10:16:27 +0530

    Create CodeKata_Ex1.java

---
+++ CodeKata_Ex1.java
@@ -0,0 +1,23 @@
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+	public static void main (String[] args) throws Exception {
+	    int n;
+	    Scanner sc = new Scanner(System.in);
+	    try{
+	        n = sc.nextInt();
+	        if(n==0)
+	        System.out.println("Zero");
+	        else if(n>0)
+	        System.out.println("Positive");
+	        if(n<0)
+	        System.out.println("Negative");
+	    }
+	    catch(Exception e){
+	       System.out.println("Invalid");
+	    }
+	}
+}
