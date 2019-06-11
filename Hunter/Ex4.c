4b54665268815dce37059d5645cbab9d12fd452f
Raj <40709274+Raj00377@users.noreply.github.com>
2019-06-11 13:40:47 +0530

    Create Ex4.c

---
+++ Hunter/Ex4.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+   int number,i,flag=0;
+   scanf("%d",&number);
+   int arr[number];
+   int a[10]= {0};
+   for(i=0;i<number;i++)
+    scanf("%d",&arr[i]);
+   for(i=0;i<number;i++)
+    a[arr[i]]++;
+   for(i=0;i<number;i++)
+    if(a[i]==1)
+     printf("%d ",i);
+    return 0;
+   
+}
