b2efd6e00faef33e405e35ecfc21c55f8388585a
Raj <40709274+Raj00377@users.noreply.github.com>
2019-06-11 10:25:29 +0530

    Create CodeKata_Ex2.java

---
+++ CodeKata_Ex2.java
@@ -0,0 +1,21 @@
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+public class CodeKata_Ex2 {
+	public static void main (String[] args) throws Exception {
+	    int n;
+	    Scanner sc = new Scanner(System.in);
+	    try{
+	        n = sc.nextInt();
+	        if(n%2==0)
+	        System.out.println("Even");
+	        else
+	        System.out.println("Odd");
+	    }
+	    catch(Exception e){
+	       System.out.println("Invalid");
+	    }
+	}
+}
