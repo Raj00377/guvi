a4e967ee79f920986a492e797e64f5ab6ec99ef3
Raj <40709274+Raj00377@users.noreply.github.com>
2019-06-11 12:05:42 +0530

    Update Ex1.c

--- Hunter/Ex1.c
+++ Hunter/Ex1.c
@@ -3,7 +3,7 @@
 
 int main()
 {
-   int number,i;
+   int number,i,flag=0;
    scanf("%d",&number);
    int arr[number];
    int a[10]={0};
@@ -12,9 +12,12 @@ int main()
    for(i=0;i<number;i++)
     a[arr[i]]++;
    for(i=0;i<number;i++)
-    if(a[i]>1)
+    if(a[i]>1){
      printf("%d ",i);
-    
+     flag=1;
+    }
+   if(flag==0)
+    printf("unique");
     return 0;
    
 }
