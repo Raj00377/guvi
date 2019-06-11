cea6ba55f85097c88152d56e83d1bba26b3af570
Raj <40709274+Raj00377@users.noreply.github.com>
2019-06-11 13:34:31 +0530

    Update Ex3.c

--- Hunter/Ex3.c
+++ Hunter/Ex3.c
@@ -2,14 +2,18 @@
 #include<stdlib.h>
 int main()
 {
-   int number,i;
+   int number,i,flag=0;
    scanf("%d",&number);
    int arr[number];
    for(i=0;i<number;i++)
     scanf("%d",&arr[i]);
    for(i=0;i<number;i++)
-    if(arr[i]==i)
+    if(arr[i]==i){
      printf("%d ",i);
+     flag=1;
+    }
+   if(flag==0)
+      printf("-1");
     return 0;
    
 }
